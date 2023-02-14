2de0c97083b9fc38320d13a17baca56c2b104725
rhammock1 <robert.hammock@gotab.io>
2023-02-13 20:28:27 -0500

    better logging

--- parent_node/parent_node.ino
+++ parent_node/parent_node.ino
@@ -318,7 +318,7 @@ void setup() {
   
   //Set device in STA mode to begin with
   WiFi.mode(WIFI_STA);
-  Serial.println("ESPNow/Multi-Child/Parent Example");
+  Serial.println("Automatic Cat Feeder --- Parent node");
   // This is the mac address of the Parent in Station Mode
   Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
   // Init ESPNow with a fallback logic
